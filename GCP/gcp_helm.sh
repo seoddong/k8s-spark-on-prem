617db5a80542f67f00e63c79d69a86e38bb2e169
seoddong <jolajoayo@gmail.com>
2024-04-07 15:33:40 +0900

    Create gcp_helm.sh

---
+++ GCP/gcp_helm.sh
@@ -0,0 +1,5 @@
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+echo 'export PATH=$PATH:/usr/local/bin' >> ~/.bashrc # PATH 업데이트
+source ~/.bashrc
